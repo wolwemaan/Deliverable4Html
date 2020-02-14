bbc805783b34ef84a27b853e212fac430fe7cf01
Your Name <you@example.com>
2020-02-13 20:34:05 -0500

    update

--- myatoi.py
+++ myatoi.py
@@ -5,14 +5,14 @@ class Solution:
     def myAtoi(self, str: str) -> int:
         max=2147483648
         #p = re.compile("\s*([-+]?\d+).*")
+        #str="      -76"
         #str="sas42"
         #str="-91283472332"
         #str="456 with words"
         #str="-2147483647"
-        j = re.match( r'\s*([-+]?\d+)\D*', str, re.M)
+        j = re.match( r'\s*([-+]?\d+)\D*', str)
         if j:
-            m = j.group(1)
-            result = int(m)
+            result = int(j.group(1))
             if result >= max-1:
                 return max-1
             elif result <= 0-max:
@@ -21,3 +21,4 @@ class Solution:
             return result
         except:
             return 0 
+        
